087f09c8f444516ef913db074c370c967df18a93
ezraholm50 <ezraholm50@gmail.com>
2017-05-21 08:50:50 +0200

    rasbian

--- nextcloud_install_production.sh
+++ nextcloud_install_production.sh
@@ -208,22 +208,22 @@ a2enmod rewrite \
 # Install PHP 7.0
 apt-get update -q4 & spinner_loading
 check_command apt-get install -y \
-    libapache2-mod-php7.0 \
-    php7.0-common \
-    php7.0-mysql \
-    php7.0-intl \
-    php7.0-mcrypt \
-    php7.0-ldap \
-    php7.0-imap \
-    php7.0-cli \
-    php7.0-gd \
-    php7.0-pgsql \
-    php7.0-json \
-    php7.0-sqlite3 \
-    php7.0-curl \
-    php7.0-xml \
-    php7.0-zip \
-    php7.0-mbstring \
+    libapache2-mod-php \
+    php-common \
+    php-mysql \
+    php-intl \
+    php-mcrypt \
+    php-ldap \
+    php-imap \
+    php-cli \
+    php-gd \
+    php-pgsql \
+    php-json \
+    php-sqlite3 \
+    php-curl \
+    php-xml \
+    php-zip \
+    php-mbstring \
     php-smbclient
 
 # Enable SMB client
